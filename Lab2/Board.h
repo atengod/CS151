197ace8db4aa80f7938abe7de2a1039066d496ec
atengod <32345524+atengod@users.noreply.github.com>
2017-11-10 10:57:40 -0500

    Update Board.h

--- Lab2/Board.h
+++ Lab2/Board.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: <Creators>
+//Author: <Michael Olorode>
+//Creation: <11/4/17>
 #ifndef BOARD_H
 #define BOARD_H
 
@@ -8,7 +8,12 @@
 
 std::string board()
 {	
-	return "";
+	std::stringstream out;
+	out << "\n" << a << " " << b << " " << c << "\n";
+	out << d << " " << e << " " << f << "\n";
+	out << g << " " << h << " " << i << "\n";
+	
+	return out.str();
 }
 
 #endif 
