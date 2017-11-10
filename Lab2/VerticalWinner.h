e65ff4442891c5faafdfc75bdfc5b1116643de52
atengod <32345524+atengod@users.noreply.github.com>
2017-11-10 10:53:29 -0500

    Update VerticalWinner.h

--- Lab2/VerticalWinner.h
+++ Lab2/VerticalWinner.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: <Creators>
+//Author: <Michael Olorode>
+//Creation: <11/4/17>
 #ifndef VERTICALWINNER_H
 #define VERTICALWINNER_H
 
@@ -8,7 +8,12 @@
 
 bool verticalWinner()
 {
-	return false;
+	bool val1 = (a == d) && (a == g) && (d == g);
+	return true;
+	bool val2 = (c == e) && (c == h) && (e == h);
+	return true;
+	bool val3 = (c == i) && (c == f) && (f == i);
+	return true;
 }
 
 #endif 
