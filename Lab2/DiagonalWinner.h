edb381ddeb710b506af09e17d02105aa9247b201
atengod <32345524+atengod@users.noreply.github.com>
2017-11-10 10:48:57 -0500

    Update DiagonalWinner.h

--- Lab2/DiagonalWinner.h
+++ Lab2/DiagonalWinner.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: <Creators>
+//Author: <Michael Olorode>
+//Creation: <11/4/17>
 #ifndef DIAGONALWINNER_H
 #define DIAGONALWINNER_H
 
@@ -8,7 +8,10 @@
 
 bool diagonalWinner()
 {
-	return false;
+	bool val = (a == e) && (a == i) && (e == i);
+	return true;
+	bool val = (c == e) && (c == g) && (g == e);
+	return true;
 }
 
 #endif 
