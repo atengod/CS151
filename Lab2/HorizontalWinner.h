1f95e928be35fffcbd24cc280cef0d8f4dc3296a
atengod <32345524+atengod@users.noreply.github.com>
2017-11-13 09:46:52 -0500

    Update HorizontalWinner.h

--- Lab2/HorizontalWinner.h
+++ Lab2/HorizontalWinner.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: <Creators>
+//Author: <Michael Olorode>
+//Creation: <11/4/17>
 #ifndef HORIZONTALWINNER_H
 #define HORIZONTALWINNER_H
 
@@ -8,7 +8,15 @@
 
 bool horizontalWinner()
 {
-	return false;
+	if ((a == b) && (a == c) && (b == c) && (a != '-') && (b != '-') && (c != '-'))
+		return true;
+	if ((d == e) && (d == f) && (e == f) && (d != '-') && (e != '-') && (f != '-'))
+		return true;
+	if ((g == h) && (g == i) && (h == i) && (g != '-') && (h != '-') && (i != '-'))
+		return true;
+	
+	else
+		return false;
 }
 
 #endif 
