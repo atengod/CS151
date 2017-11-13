f9e44974a0ae2f57d88896e4c6fc692890faf728
atengod <32345524+atengod@users.noreply.github.com>
2017-11-13 18:39:23 -0500

    Lab 4

---
+++ Displacement.h
@@ -0,0 +1,14 @@
+//Team: <Team Name>
+//Author: <Names>
+//Creation: <Date>
+#ifndef DISPLACEMENT_H
+#define DISPLACEMENT_H
+
+#include "Archive.h"
+
+double displacement(double v,double t,double a)
+{
+	return 0.0;
+}
+
+#endif 
---
+++ Velocity.h
@@ -0,0 +1,14 @@
+//Team: <Team Name>
+//Author: <Names>
+//Creation: <Date>
+#ifndef VELOCITY_H
+#define VELOCITY_H
+
+#include "Archive.h"
+
+double velocity(double v,double t,double a)
+{	
+	return 0.0;
+}
+
+#endif 
