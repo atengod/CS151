4354765346339e5eca0265993ec08885cf05dd9c
atengod <32345524+atengod@users.noreply.github.com>
2017-11-13 10:01:55 -0500

    Update MakeMove.h

--- Lab2/MakeMove.h
+++ Lab2/MakeMove.h
@@ -8,6 +8,21 @@
 
 bool makeMove(int position,char token)
 {
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (a == '-')
+			{
+				a = token;
+				return true;
+			}
+			esle
+			{
+				return false;
+			}
+		}
+	}
 	return false;
 }
 
