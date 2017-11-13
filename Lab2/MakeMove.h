808ea753c260dbf577883ee346c1a9cae899a820
atengod <32345524+atengod@users.noreply.github.com>
2017-11-13 18:45:02 -0500

    Update MakeMove.h

--- Lab2/MakeMove.h
+++ Lab2/MakeMove.h
@@ -17,7 +17,135 @@ bool makeMove(int position,char token)
 				a = token;
 				return true;
 			}
-			esle
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (b == '-')
+			{
+				b = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (c == '-')
+			{
+				c = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (d == '-')
+			{
+				d = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (e == '-')
+			{
+				e = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (f == '-')
+			{
+				f = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (g == '-')
+			{
+				g = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (h == '-')
+			{
+				h = token;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	return false;
+	if (position>= 1 && position<=9)
+	{
+		if (position == 1)
+		{
+			if (i == '-')
+			{
+				i = token;
+				return true;
+			}
+			else
 			{
 				return false;
 			}
