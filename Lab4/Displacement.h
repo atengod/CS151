2d15c5a5d8e2b19eb8c9d3362bb895fd0984206e
atengod <mykaelolawale@gmail.com>
2017-11-13 20:35:41 -0500

    Lab Folders

---
+++ Lab4/Displacement.h
@@ -0,0 +1,15 @@
+//Team: <Team Name>
+//Author: <Names>
+//Creation: <Date>
+#ifndef DISPLACEMENT_H
+#define DISPLACEMENT_H
+
+#include "Archive.h"
+
+double displacement(double v,double t,double a)
+{
+	double displacement = h + 1/2 (v + v)t
+		return 0.0;
+}
+
+#endif 
---
+++ Lab4/Velocity.h
@@ -0,0 +1,15 @@
+//Team: <Team Name>
+//Author: <Names>
+//Creation: <Date>
+#ifndef VELOCITY_H
+#define VELOCITY_H
+
+#include "Archive.h"
+
+double velocity(double v,double t,double a)
+{	
+	double velocity = v + a * t
+		cout << final velocity;
+}
+
+#endif 
