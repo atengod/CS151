6185cb760a1c02d24d5dca2bab3f682c94fe3555
atengod <32345524+atengod@users.noreply.github.com>
2017-11-15 17:57:44 -0500

    Update Board.h

--- Lab3/Board.h
+++ Lab3/Board.h
@@ -7,8 +7,27 @@
 #include "Archive.h"
 
 std::string board()
-{	
-	return "";
+{
+	std::string result = "\n";
+	result += grid[0];
+	result += " ";
+	result += grid[1];
+	result += " ";
+	result += grid[2];
+	result += "\n";
+	result += grid[3];
+	result += " ";
+	result += grid[4];
+	result += " ";
+	result += grid[5];
+	result += "\n";
+	result += grid[6];
+	result += " ";
+	result += grid[7];
+	result += " ";
+	result += grid[8];
+	result += "\n";
+	return result;
 }
 
 #endif 
