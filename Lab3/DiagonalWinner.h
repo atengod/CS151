75c1c8dfd2962aa2f5ab265f643d3935ed0b1527
atengod <32345524+atengod@users.noreply.github.com>
2017-11-15 17:58:18 -0500

    Update DiagonalWinner.h

--- Lab3/DiagonalWinner.h
+++ Lab3/DiagonalWinner.h
@@ -8,6 +8,15 @@
 
 bool diagonalWinner()
 {
+	if ((grid[4] == grid[2] && grid[4] == grid[6] && grid[6] == grid[2]) && (grid[2] != '-') && (grid[4] != '-') && (grid[6] != '-'))
+	{
+		return true;
+	}
+	else if ((grid[4] == grid[0] && grid[4] == grid[8] && grid[0] == grid[8]) && (grid[0] != '-') && (grid[4] != '-') && (grid[8] != '-'))
+	{
+		return true;
+	}
+
 	return false;
 }
 
