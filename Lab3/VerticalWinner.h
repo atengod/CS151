367e897e295ddd8febaf24880b8d817d9c94ebac
atengod <32345524+atengod@users.noreply.github.com>
2017-11-15 10:12:12 -0500

    Update VerticalWinner.h

--- Lab3/VerticalWinner.h
+++ Lab3/VerticalWinner.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: <Creators>
+//Author: <Michael Olorode>
+//Creation: <11/9/17>
 #ifndef VERTICALWINNER_H
 #define VERTICALWINNER_H
 
