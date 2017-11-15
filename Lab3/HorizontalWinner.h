bc6a989da6385653a49d45204ccb31ceb53ba9ae
atengod <32345524+atengod@users.noreply.github.com>
2017-11-15 10:08:59 -0500

    Update HorizontalWinner.h

--- Lab3/HorizontalWinner.h
+++ Lab3/HorizontalWinner.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: <Creators>
+//Author: <Michael Olorode>
+//Creation: <11/9/17>
 #ifndef HORIZONTALWINNER_H
 #define HORIZONTALWINNER_H
 
