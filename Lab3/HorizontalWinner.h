6bc949db6de902796bcbfe432dcb1d132ef22f9d
atengod <32345524+atengod@users.noreply.github.com>
2017-11-15 18:29:10 -0500

    Update HorizontalWinner.h

--- Lab3/HorizontalWinner.h
+++ Lab3/HorizontalWinner.h
@@ -8,7 +8,18 @@
 
 bool horizontalWinner()
 {
-	return false;
-}
+	if ((grid[0] == grid[1] && grid[0] == grid[2] && grid[1] == grid[2]) && (grid[0] != '-') && (grid[1] != '-') && (grid[2] != '-'))
+	{
+		return true;
+	}
+	else if ((grid[3] == grid[4] && grid[3] == grid[5] && grid[4] == grid[5]) && (grid[3] != '-') && (grid[4] != '-') && (grid[5] != '-'))
+	{
+		return true;
+	}
+	else if ((grid[6] == grid[7] && grid[6] == grid[8] && grid[7] == grid[8]) && (grid[6] != '-') && (grid[7] != '-') && (grid[8] != '-'))
+	{
+		return true;
+	}
+	return false;}
 
 #endif 
