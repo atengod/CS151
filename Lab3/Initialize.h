44d69bf83cce3a0c938f9ced3610cc3fcc9e7d47
atengod <32345524+atengod@users.noreply.github.com>
2017-11-15 18:41:21 -0500

    Update Initialize.h

--- Lab3/Initialize.h
+++ Lab3/Initialize.h
@@ -8,7 +8,15 @@
 
 void initialize()
 {
-	return;
+	grid[0] += '-';
+	grid[1] += '-';
+	grid[2] += '-';
+	grid[3] += '-';
+	grid[4] += '-';
+	grid[5] += '-';
+	grid[6] += '-';
+	grid[7] += '-';
+	grid[8] += '-';
 }
 
 #endif 
