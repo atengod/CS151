34beddfdc5247c49192be6328c8d5ca178c1872b
atengod <32345524+atengod@users.noreply.github.com>
2017-11-22 20:56:28 -0500

    Update assign6.py

--- Assignment 6/assign6.py
+++ Assignment 6/assign6.py
@@ -1,3 +1,32 @@
+/*********************
+//Name:Michael Olorode
+//Creation: 11/20/17
+//Version: Assignment 6
+*********************/
+
+/***********************************
+//Name: copy()
+//Parameter(s): string filename;
+//Return: nothing
+//Description: Opens a file and copies its content to a new file.
+// Label             Task                                            Goto
+// 01                /print(Enter filename)/                          02
+// 02                /cin = filename/                                03
+// 03                [copy(filename)]                                04
+// 04                (exit)
+***************************************/
+///////////////////////////////////////////////////////
+/****************************************
+// Label             Task                                            Goto
+// 01                [in = filename]                                 02
+// 02                [out = stuff.txt]                               03
+// 03                <getline(in,str) != eof()>                      04,06 
+// 04                [out.print(str + "\n")]                          03
+// 05                [out.close()]                                   06
+// 06                [in.close()]                                    07
+// 08                (exit)
+***************************************/
+
 #include<iostream>
 #include<fstream>
 using namespace std;
